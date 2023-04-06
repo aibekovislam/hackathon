990dbefff6de0c3dc26927a6ae59b93af19c52aa
Islam <islamxdaamduu@gmail.com>
2023-04-06 20:37:52 +0600

    added navbar

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,9 +1,35 @@
 import React from 'react'
+import '../static/style.css'
+import { ReactComponent as LogoSvg } from '../static/Filmoon.svg';
+import { ReactComponent as DropDownSvg } from '../static/Vector 1.svg';
+import { ReactComponent as GlobusSvg } from '../static/Vector (2).svg';
 
 function Navbar() {
   return (
-    <div>
-        
+    <div className='black'>
+        <div className='container navbar'>
+            <div className='navbar__items'>
+                <div className='navbar__item'>
+                    <div>
+                        <LogoSvg/>
+                    </div>
+                </div>
+                <ul className='navbar__item'>
+                    <li className='item'>
+                        <div className='language_btn'>
+                            <button>
+                                <DropDownSvg/>
+                                    English
+                                <GlobusSvg/>
+                            </button>
+                        </div>
+                    </li>
+                    <li className='item'>
+                        <button className='sign_in_btn'>Sign In</button>
+                    </li>
+                </ul>
+            </div>
+        </div>
     </div>
   )
 }
