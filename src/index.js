f3bf4dc239f32a2132ab894b4d916b59ae98384d
Eldar <126486162+Epimetheuc@users.noreply.github.com>
2023-04-07 17:41:00 +0600

    added height vh

--- src/index.js
+++ src/index.js
@@ -3,13 +3,11 @@ import App from "./App";
 import ReactDOM from "react-dom";
 import { BrowserRouter } from "react-router-dom";
 // import Footer from "./components/Footer"
-import MyFooter from "./components/footer";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 
 root.render(
   <BrowserRouter>
     <App />
-    <MyFooter />
   </BrowserRouter>
 );
--- src/layouts/MainLayout.jsx
+++ src/layouts/MainLayout.jsx
@@ -1,21 +1,23 @@
-import React from 'react'
-import Navbar from '../components/Navbar';
-import { Outlet } from 'react-router-dom';
-import '../static/style.css'
+import React from "react";
+import Navbar from "../components/Navbar";
+import { Outlet } from "react-router-dom";
+import "../static/style.css";
+// import Footer from "../components/Footer";
+import MyFooter from "../components/footer";
 
 function MainLayout() {
   return (
     <>
       <div className="mainBlock">
-        <Navbar/>
-        <Outlet/>
+        <Navbar />
+        <Outlet />
       </div>
-      <div className='mainBlockMobile'>
-        <Navbar/>
-        <Outlet/>
+      <div className="mainBlockMobile">
+        <Navbar />
+        <Outlet />
       </div>
     </>
-  )
+  );
 }
 
-export default MainLayout
+export default MainLayout;
