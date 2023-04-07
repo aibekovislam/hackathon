cc91bc030e49ce09d0792e03eae7cc80da37a6cc
Eldar <126486162+Epimetheuc@users.noreply.github.com>
2023-04-07 19:31:26 +0600

    pikachu

--- src/components/leaveSoon.jsx
+++ src/components/leaveSoon.jsx
@@ -1,8 +1,17 @@
 import React from "react";
 import "../static/leaveSoon.css";
 
-function leaveSoon() {
-  return <div>leaveSoon</div>;
+function LeaveSoon() {
+  return (
+    <div>
+      <div class="pc">
+        <img src={require("../static/bc-leave.png")} alt="" class="bc" />
+      </div>
+      <div class="mobile">
+        <img src={require("../static/bc-leave.png")} class="bg_mobile" />
+      </div>
+    </div>
+  );
 }
 
-export default leaveSoon;
+export default LeaveSoon;
--- src/routes/MainRoutes.jsx
+++ src/routes/MainRoutes.jsx
@@ -1,17 +1,19 @@
-import React from 'react'
-import { Route, Routes } from 'react-router-dom'
-import MainLayout from '../layouts/MainLayout'
-import HomePage from '../pages/HomePage'
-import MainScreen from '../pages/MainScreen'
-import '../static/style.css'
+import React from "react";
+import { Route, Routes } from "react-router-dom";
+import MainLayout from "../layouts/MainLayout";
+import HomePage from "../pages/HomePage";
+import MainScreen from "../pages/MainScreen";
+import LeaveSoon from "../components/leaveSoon";
+import "../static/style.css";
 
 function MainRoutes() {
   return (
     <Routes>
-        <Route element={<MainLayout/>}>
-          <Route path='/' element={<HomePage/>}></Route>
-        </Route>
-        <Route path='/main' element={<MainScreen/>}/>
+      <Route element={<MainLayout />}>
+        <Route path="/" element={<HomePage />}></Route>
+      </Route>
+      <Route path="/main" element={<MainScreen />} />
+      <Route path="/leaving-so-soon" element={<LeaveSoon />} />
     </Routes>
   );
 }
